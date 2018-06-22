9155a5e38995ecdb086291ca0b48f543dc3c059c
Zszywka <32071894+Zszywka@users.noreply.github.com>
2018-06-22 11:55:51 +0200

    Update App.js
    Delete comments in App.js

--- components/App.js
+++ components/App.js
@@ -1,61 +1,40 @@
-// tutaj tradycyjnie wszystko będziemy wiązać w jeden byt.
-// W tym komponencie będziemy odbierać wiadomość z komponentu zajmującego się
-// wyszukiwaniem i przekazywać do komponentu, który wyświetli odpowiedniego GIFa
-// pobieranie Gif
 var GIPHY_API_URL = 'https://api.giphy.com';
-var GIPHY_PUB_KEY = 'q4k1xiFfyXHFEVGeG4SYB8VJsIA5OtQu'; //???
+var GIPHY_PUB_KEY = 'q4k1xiFfyXHFEVGeG4SYB8VJsIA5OtQu'; 
 
 App = React.createClass({
-// initial values of the state
   getInitialState(){
     return {
       loading: false,
-      // key with component Search.js
       searchingText: '',
       gif: {}
     };
   },
 
-  // pobierz na wejsciu wpisany tekst
   handleSearch: function(searchingText) {
     this.setState({
-      // zasygnalizuj ze zaczol sie proces ladowania
       loading: true
     });
-    // rozpocznij pobieranie gifa
     this.getGif(searchingText, function(gif) {
-      // na zakonczenie pobierania
       this.setState({
-        // przestan sygnalizowac ladowanie
         loading: false,
-        // ustaw nowego gifa z wyniku pobierania
         gif: gif,
-        // ustaw nowy stan dla wyszukiwanego tekstu
         searchingText: searchingText
       });
-      // this.getGif()-> wskazuje na cos innego niz komponent App,
-      // metoda bind obejdzie to i zachowa kontekst, wskaze na gifa
     }.bind(this));
   },
 
-  // na wejscie przyjmujemy parametr tekst wpisany, funkcja ktora ma sie wykonac po pobraniu
   getGif: function(searchingText, callback) {
-    // kontruujemy adres URL dla API Giphy
     var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-    // wywolujemy cala sekwencje tworzenia zapytania do XHR do serwera i wysylamy je
     var xhr = new XMLHttpRequest();
 
     xhr.open('GET', url);
     xhr.onload = function() {
       if (xhr.status === 200) {
-        // w obiekcie odpowiedzi mamy obiekt z danymi, rozpakkowujemy je do zmiennej data
         var data = JSON.parse(xhr.responseText).data;
-        // ukladamy obiekt gif na podstawie tego co przyslala serwer
         var gif = {
           url: data.fixed_width_downsampled_url,
           sourceUrl: data.url
         };
-        // ten obiekt przekazujemy do funkcji callback, a callback to 2 parametr getGif
         callback(gif);
       }
     };
